8bf57e7e80072421a4511fd4d9fa4ab469752e10
mat dup <mathiasdupre@gmail.com>
2019-08-19 15:59:34 +0200

    initialize arenas controlller and views

---
+++ app/controllers/arenas_controller.rb
@@ -0,0 +1,45 @@
+class ArenasController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @arenas = Arena.all
+  end
+
+  def show
+  end
+
+  def new
+    @arena = Arena.new
+  end
+
+  def create
+    @arena = Arena.new(arena_params)
+    @arena.save
+    redirect_to arena_path(@arena)
+  end
+
+  def edit
+  end
+
+  def update
+    @arena.update(arena_params)
+    redirect_to arena_path(@arena)
+  end
+
+  def destroy
+    @arena.destroy
+    redirect_to arenas_path
+  end
+
+  private
+
+  def arena_params
+    params.require(:arena).permit(:name, :address, :stars)
+  end
+
+  def set_arena
+    @arena = Arena.find(params[:id])
+  end
+end
+

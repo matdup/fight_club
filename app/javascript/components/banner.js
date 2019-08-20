2a8fc3b814dc49669ac31032d827429979f968f5
Quentin Debray <qtn.dbr@gmail.com>
2019-08-20 17:52:23 +0200

    decommented the JS

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,11 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  // new Typed('#banner-typed-text', {
-  //   strings: ["Change your life", "Learn to fight"],
-  //   typeSpeed: 100,
-  //   loop: true
-  // });
+  new Typed('#banner-typed-text', {
+    strings: ["Change your life", "Learn to fight", "WHAT THE **** IS HAPPENING"],
+    typeSpeed: 100,
+    loop: true
+  });
 }
 
 export { loadDynamicBannerText };

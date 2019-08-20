03c131eddc753990acbf9fd96f565356db1d95e0
michael boutelet <michael.boutelet@outlook.fr>
2019-08-20 15:36:28 +0200

    modal review done by michael, first version

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
 import "bootstrap";
+import { reviewArena } from "../components/review_arena";
+import { previewImageOnFileSelect } from "../components/preview_photo";
+
+reviewArena();
+previewImageOnFileSelect();
+

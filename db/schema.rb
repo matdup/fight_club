67245e96af6055f935335334fa35a33d21ace7d4
Quentin Debray <qtn.dbr@gmail.com>
2019-08-20 16:47:31 +0200

    corrected the home page

--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,11 @@ ActiveRecord::Schema.define(version: 2019_08_20_110708) do
   create_table "arenas", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.string "photorails"
     t.integer "price"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_arenas_on_user_id"
   end
 

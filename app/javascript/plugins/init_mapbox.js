315a373453b5ce192a501ee8072d6589e7254bfc
mat dup <mathiasdupre@gmail.com>
2019-08-21 13:40:34 +0200

    searchbar done

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -31,6 +31,7 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
   }
 };
 

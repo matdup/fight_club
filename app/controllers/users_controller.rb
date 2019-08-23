a38dbf5d0fa190efb91a262838fc94ace2cebdb4
Wadi BEN MIMOUN <bmnwa@outlook.com>
2019-08-23 14:43:35 +0200

    adding methods for the dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def dashboard
     @arenas = current_user.arenas
     @bookings = current_user.bookings
+    @my_bookings = current_user.bookings_as_owner
   end
 end
